77f990935628560938bb0f294b0b9804d3145b4a
rsbarrera <romel.barrer@global-health.com>
2024-07-09 16:44:32 +0800

    Added unit test for UserProfilesController

---
+++ RunningActivity.Tests/Controllers/BaseControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using RunningActivity.API.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningActivity.Tests.Controllers
+{
+    public  abstract class BaseControllerTest
+    {
+        protected static IMapper _mapper;
+        protected BaseControllerTest()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new UsersMappingProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+    }
+}
---
+++ RunningActivity.Tests/Controllers/UserProfilesControllerShould.cs
@@ -0,0 +1,57 @@
+﻿
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using NLog;
+using RunningActivity.API.Controllers;
+using RunningActivity.API.Models;
+using RunningActivity.API.Profiles;
+using RunningActivity.Domain.Entities;
+using RunningActivity.Infrastructure.Services;
+using ILogger = NLog.ILogger;
+
+namespace RunningActivity.Tests.Controllers
+{
+    public class UserProfilesControllerShould:BaseControllerTest
+    {
+        private readonly Mock<IUserProfileService> _mockUserProfileService;
+        private readonly Mock<ILogger<UserProfilesController>> _mockLogger;
+        private readonly UserProfilesController _sut;
+
+        public UserProfilesControllerShould()
+        {
+            _mockUserProfileService = new Mock<IUserProfileService>();
+            _mockLogger = new Mock<ILogger<UserProfilesController>>();
+            _sut = new UserProfilesController(
+                _mockUserProfileService.Object,
+                _mapper,
+                _mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task GetAllProfiles()
+        {
+            //Arrange
+            var userProfiles = new List<UserProfile>
+            {
+                new UserProfile { Id = 1, Name = "User1",Weight = 60,Height = 171,BirthDate = DateTime.Now},
+                new UserProfile { Id = 2, Name = "User2",Weight = 62,Height = 175,BirthDate = DateTime.Now }
+            };
+
+            _mockUserProfileService.Setup(x => x.GetAllProfilesAsync())
+                .ReturnsAsync(userProfiles);
+
+            //Act
+            var result = await _sut.GetAllProfiles();
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsType<List<UserProfileDto>>(okResult.Value);
+            Assert.Equal(2, returnValue.Count);
+
+        }
+    }
+}
